b541051eb4f288fbcb24462705482c474dd8aeaa
Julia <sidorchuk1991@internet.ru>
2022-08-20 16:57:29 +0300

    Маша

---
+++ Examples 006 Liker/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВедите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
