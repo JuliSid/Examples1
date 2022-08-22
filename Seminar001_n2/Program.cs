de8ace0684cb94a81db723356b09fc4576161ca4
Julia <sidorchuk1991@internet.ru>
2022-08-22 11:46:44 +0300

    SQR

---
+++ Seminar001_n2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int sqr = numberA * numberA;
+Console.Write("Квадрат числа равен ");
+Console.WriteLine(sqr);
