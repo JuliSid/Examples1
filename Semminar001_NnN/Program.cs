f113c85e8e7aa7cc3dbbcb96fac84072e7bbf6f0
Julia <sidorchuk1991@internet.ru>
2022-08-22 17:01:59 +0300

    ot -N do N

---
+++ Semminar001_NnN/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int numberNeg = number * (-1);
+
+while(numberNeg <= number)
+{
+    Console.WriteLine(numberNeg);
+    numberNeg++;
+}
