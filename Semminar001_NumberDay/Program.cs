b62c9b8a957033b556cc1e97e5b71670eccc39f8
Julia <sidorchuk1991@internet.ru>
2022-08-22 16:24:30 +0300

    NumberDay

---
+++ Semminar001_NumberDay/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число ");
+int NumberDay = Convert.ToInt32(Console.ReadLine());
+
+if(NumberDay == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(NumberDay == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(NumberDay == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(NumberDay == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(NumberDay == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(NumberDay == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(NumberDay == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if(NumberDay < 1 || NumberDay > 7)
+{
+    Console.WriteLine("Ошибка! Введите число от 1 до 7");
+}
