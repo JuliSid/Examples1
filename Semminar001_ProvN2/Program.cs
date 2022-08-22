79b54433900d081e6c7fb3f5615f847a7ba1ea33
Julia <sidorchuk1991@internet.ru>
2022-08-22 12:16:07 +0300

    Proverka SQR

---
+++ Semminar001_ProvN2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberZ = Convert.ToInt32(Console.ReadLine());
+ 
+ if(numberA == numberZ * numberZ)
+ {
+    Console.WriteLine("Первое число является квадратом второго");
+
+ }
+ else
+ {
+    Console.WriteLine("Первое число не является квадратом второго");
+ }
