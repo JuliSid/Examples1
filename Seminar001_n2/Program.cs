e170a7cfb6796e03f676600714c4987b9dc25fd3
Julia <sidorchuk1991@internet.ru>
2022-08-23 20:13:22 +0300

    Dopzadacha

--- Seminar001_n2/Program.cs
+++ Seminar001_n2/Program.cs
@@ -3,3 +3,34 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 int sqr = numberA * numberA;
 Console.Write("Квадрат числа равен ");
 Console.WriteLine(sqr);
+
+
+
+// using System;
+// using System.Linq;
+ 
+// namespace ConsoleApp112
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Write($"Кодичество чисел для ввода: ");
+//             int N = Convert.ToInt32(Console.ReadLine());
+ 
+//             int[] Arr = new int[N];
+ 
+//             for (int i = 0; i < Arr.Length; i++)
+//             {
+//                 Console.Write($"{i+1}-й элемент: ");
+//                 Arr[i] = Convert.ToInt32(Console.ReadLine());
+//             }
+ 
+//             Console.WriteLine($"Входной массив {String.Join(", ", Arr)}");
+ 
+//             int MaxElementInArr = Arr.Max();
+ 
+//             Console.WriteLine($"Максимальный элемент массива из введенных: {MaxElementInArr}");
+//         }
+//     }
+// 
---
+++ SemminarDZ1/ZadachaN7Lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на входе принимает трехзначное число и выводить последнее цифру в нем.
+
+Console.WriteLine("Введите трехзназное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100 || number > 999)
+{
+Console.WriteLine("Число не трехзначное");
+}
+
+else
+{
+    number = number%10;
+    Console.WriteLine("Последняя цифра числа - "+ number);
+}
