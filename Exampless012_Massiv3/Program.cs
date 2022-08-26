712b7839e126cbbf5e7e282ecc8fa34048e6d9e5
Julia <sidorchuk1991@internet.ru>
2022-08-26 21:50:27 +0300

    Massiv

---
+++ Exampless012_Massiv3/Program.cs
@@ -0,0 +1,27 @@
+﻿void FiiArray(int[] colection)
+{
+    int length = colection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        colection[index]=new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count =col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int[] array = new int[10];
+
+FiiArray(array);
+PrintArray(array);
