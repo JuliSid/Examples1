07984d1d68e82335da1316f0970f692cc2b53261
Julia <sidorchuk1991@internet.ru>
2022-08-26 23:08:57 +0300

    Кратность чисел

--- Lessons/Examples 001 HW/Program.cs
+++ Lessons/Examples 001 HW/Program.cs
@@ -1,7 +1,1 @@
-﻿int num = new Random().Next(100, 1000);
-int nun1 = num/100;
-int num2 = num%10;
-
-Console.WriteLine(num);
-Console.Write(num1);
-Console.Write(num2);
+﻿Console.WriteLine("Hello, World!");
