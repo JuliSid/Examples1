ca60def7ac9dc78a37ebe78be7a24a6eafa980e7
Julia <sidorchuk1991@internet.ru>
2022-08-26 22:33:33 +0300

    Второе число минус

--- Lessons/Examples 001 HW/Program.cs
+++ Lessons/Examples 001 HW/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int num = new Random().Next(100, 1000);
+int nun1 = num/100;
+int num2 = num%10;
+
+Console.WriteLine(num);
+Console.Write(num1);
+Console.Write(num2);
