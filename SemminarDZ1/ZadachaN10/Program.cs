9c30167be71ae481b35600471648ba59fa137510
Julia <sidorchuk1991@internet.ru>
2022-08-27 10:59:35 +0300

    Задача 10 минус второе число

---
+++ SemminarDZ1/ZadachaN10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num/100;
+int num3 = num%10;
+int num2 =(num-(num1*100)-num3)/10;
+
+if(num > 100 && num < 1000)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine("Ошибка! Введите трехзначное число");
+}
