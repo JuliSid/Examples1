6968321e7329b3eea0816b7e7ac26f3a8d734cac
Julia <sidorchuk1991@internet.ru>
2022-08-27 17:44:22 +0300

    Push

---
+++ SeminarDZ2/ZadachaN13/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите ваше число ");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = num.ToString();
+Console.WriteLine(str[2]);
---
+++ SeminarDZ2/ZadachaN15/Program.cs
@@ -0,0 +1,43 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число ");
+int numDate = Convert.ToInt32(Console.ReadLine());
+if(numDate == 1)
+{
+    Console.WriteLine("Понедельник. До выходного еще далеко..");
+
+}
+if(numDate == 2)
+{
+    Console.WriteLine("Вторник. До выходного все еще далеко.");
+
+}
+if(numDate == 3)
+{
+    Console.WriteLine("Среда. Уже прошли половину, но все же далеко.");
+
+}
+if(numDate == 4)
+{
+    Console.WriteLine("Четверг. Уже совсем чуть-чуть");
+
+}
+if(numDate == 5)
+{
+    Console.WriteLine("Пятница. Вы на финишной прямой");
+
+}
+if(numDate == 6)
+{
+    Console.WriteLine("Суббота. УРА! Выходной.");
+
+}
+if(numDate == 7)
+{
+    Console.WriteLine("Воскресенье. УРА! Выходной.");
+
+}
+if(numDate < 1 || numDate > 7)
+{
+    Console.WriteLine("Ошибка! Введите другое число");
+}
