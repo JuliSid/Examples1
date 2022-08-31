5e1b93bc27848b1dbc6aff50d6a219b3603d0824
Julia <sidorchuk1991@internet.ru>
2022-08-31 21:36:09 +0300

    Zadacha21

---
+++ SeminarDZ3/ZadachaN19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите  число");
+string str = Console.ReadLine();
+int length = str.Length;
+
+for(int i = 0; i < length; i++)
+{
+    Console.WriteLine(str[length - i - 1]);
+    str == str2;
+}
---
+++ Semmnar/seminar3TablPow/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+// int count =1;
+
+// while(count<=N)
+// {
+//     Console.WriteLine(Math.Pow(count,2));
+//     count++;
+// }
+
+for (int count = 1; count<=N; count++) {
+double result = Math.Pow(count, 2);
+Console.WriteLine(result);
+}
