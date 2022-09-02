b566a7879ea744faf49e76fbc83ef8f32437dd09
Julia <sidorchuk1991@internet.ru>
2022-09-02 23:59:45 +0300

    Seminar4

---
+++ Semmnar/Seminar4SunNum/Program.cs
@@ -0,0 +1,17 @@
+﻿// GПолучили число и на выходе получили сумму из чисел до этого числа
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма чисел равна = "+ GetSum(num));
+
+int GetSum(int number)
+{
+    int count = 0;
+    int sum = 0;
+    while(count < number)
+    {
+        count++;
+        sum+=count;  // sum=sum+count;
+    }
+    return sum;
+}
