62e8803394fa9be8304224327c781db7010592b9
Julia <sidorchuk1991@internet.ru>
2022-09-03 01:11:44 +0300

    Seminar4

---
+++ Semmnar/Seminar4Count/Program.cs
@@ -0,0 +1,39 @@
+﻿// Принимает число и на выход получает количество цифр в числе
+
+// Решение 1
+
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string result = Convert.ToString(num);
+// Console.WriteLine("Количество цифр в числе " +CountNum(result));
+
+// int CountNum(string number)
+// {
+//     int size = number.Length;
+//     return size;
+// }
+
+
+
+
+// Решение 2
+
+
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество цифр в числе " +CountNum(num));
+
+
+int CountNum(int number)
+{
+    int count = 0;
+    
+    while(number > 0)
+    {
+        count++;
+        number/=10;
+    
+    }
+    return count;
+}
---
+++ Semmnar/Seminar4Prod/Program.cs
@@ -0,0 +1,41 @@
+﻿// Принимает число и выдает произведение чисел от 1 до N
+  
+
+
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Произведение чисел = " + GetProd(num));
+
+
+int GetProd( int number)
+{
+    int count = 1;
+    int prod = 1;
+    while(count < number)
+    {
+        count++;
+        prod*=count;
+    }
+    return prod;
+}
+
+
+
+// Решение 2 ??? нЕ решается
+
+
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Произведение чисел = " + GetProd(num));
+
+
+// int GetProd( int number)
+// {
+//     int prod=1;
+//     for(int i=0; i<= number; i++)
+//     {
+//         prod*=number;
+//     }
+//     return prod;
+// }
